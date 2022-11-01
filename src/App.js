92ff4f9a293ca677e8850c302d664e8ef464aa38
tinatatana <109757952+tinatatana@users.noreply.github.com>
2022-11-01 20:38:21 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Kristina Poliukkhovych and is{" "}
+          <a href="https://github.com/tinatatana/weather-react">
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
