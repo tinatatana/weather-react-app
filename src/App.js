94c060726192c126415a8cd9ebd0c6a14e87bb10
tinatatana <109757952+tinatatana@users.noreply.github.com>
2022-11-04 00:41:56 +0100

    API integration

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <h1>Weather App</h1>
         <Weather defaultCity="Kyiv" />
         <footer>
-          This project was coded by Kristina Poliukkhovych and is{" "}
+          This project was coded by Kristina Poliukhovych and is{" "}
           <a href="https://github.com/tinatatana/weather-react">
             open-sourced on GitHub
           </a>
