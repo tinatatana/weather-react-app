661a795e0ed4208fff1403569178928a1c57449f
tinatatana <109757952+tinatatana@users.noreply.github.com>
2022-11-03 02:19:26 +0100

    added css, added axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,46 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+
+import axios from "axios";
 import "./Weather.css";
-export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
+
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      description: response.data.weather[0].description,
+      date: "Wednesday, 03.11.2022",
+      iconUrl: "https://cdn-icons-png.flaticon.com/512/2580/2580627.png",
+      city: response.data.name,
+    });
+  }
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city name"
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
         <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city name"
-              className="form-control"
-            />
+          <h1>{weatherData.city}</h1>
+          <div className="col-6">
+            <ul>
+              <li>{weatherData.date}</li>
+              <li className="text-capitalize">{weatherData.description}</li>
+              <li>
+                {" "}
+                <img
+                  src={weatherData.iconUrl}
+                  alt={weatherData.description}
+                  className="img-fluid"
+                />{" "}
+              </li>
+            </ul>
           </div>
-          <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+          <div className="col-6">
+            <ul>
+              <li>Temperature: {Math.round(weatherData.temperature)}°C</li>
+              <li>Humidity: {weatherData.humidity} %</li>
+              <li>Wind: {weatherData.wind} km/h</li>
+            </ul>
           </div>
-        </div>
-      </form>
-      <div className="row">
-        <h1>Kyiv</h1>
-        <div className="col-6">
-          <ul>
-            <li>01.11.2022</li>
-            <li>Sunny</li>
-            <li>
-              {" "}
-              <img
-                src="https://cdn-icons-png.flaticon.com/512/2580/2580627.png"
-                alt="sunny"
-                className="img-fluid"
-              />{" "}
-            </li>
-          </ul>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Temperature: 15°C</li>
-            <li>Humidity: 80</li>
-            <li>Wind: 10</li>
-          </ul>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "752a3b7c84950701295b5755aa7ad901";
+
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading";
+  }
 }
